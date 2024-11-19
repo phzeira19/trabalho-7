43d71f3a1210641b63875d8c1089fc230cfd4570
rapha <souzar14121@gmail.com>
2024-11-19 18:48:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function detecta(event){
+       let tecla = event.key;
+       if (tecla =="a" )
+       alert("você apertou 'a'")
+
+       if (tecla =="e" )
+       alert("você apertou 'e'")
+
+       if (tecla =="i" )
+       alert("você apertou 'i'")
+
+       if (tecla =="o" )
+       alert("você apertou 'o'")
+
+       if (tecla =="o" )
+       alert("você apertou 'u'")
+
+       if (tecla =="u" )
+       alert("você apertou 'o'")
+}
+
+<p id="hide" style="display: block;">posso sumir!</p>
+
+document.getElementById("hide").style.display = "none" ;
